669e284f5097c9afde5a47db3c61789b7e5ff0e1
rai-n <neeraj.rai@city.ac.uk>
2021-12-22 01:31:32 +0000

    Indeed.com data csv file generated and selenium script
    Still some todos for @rain [Neeraj] Will update in the next push

--- Selenium webdriver.py
+++ Selenium webdriver.py
@@ -4,48 +4,50 @@ from selenium.common.exceptions import NoSuchElementException
 
 import pandas as pd 
 
-#specify driver path
+# Chrome driver path that is needed for Selenium
 PATH = r'D:\Python\chromedriver_win32\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
-import time 
-
 # Results page for software engineer jobs, that are also remote 
-driver.get("https://uk.indeed.com/jobs?q=software%20engineer&l=london&remotejob")
- 
 
 # Data we are collecting 
 companies=[]
 titles=[]
 salaries=[]
 addresses=[]
-# Boolean for part time or not, 0 being part time 1 for full time after cleaning
-workingHours=[]
-remote=[]
-reviews=[]
+# remote=[] Temporarily commenting this since remote state needs to be read from Location
+ratings=[]
 jobDescription=[]
 
-descriptions=[]
+# @Todo for next push [Neeraj]
+# Boolean for part time or not, 0 being part time 1 for full time after cleaning
+workingHours=[] 
+jobDescriptionLong=[] # seeing if longer job descriptions have salary
 
-time.sleep(3000)
+# url loads 50 results of software jobs in London 
+# loop is used to step through the number of results, [50, 100, 150, etc]
+# 500 job results are checked with (0, 10) since 50 results of 10 pages will be checked
 
-jobs = driver.find_elements_by_class_name('tapItem')
-for j in jobs:
-    companies.append(j.find_element_by_class_name('companyName').text)
-    titles.append(j.find_element_by_class_name('jobTitle').text)
-    addresses.append(j.find_element_by_class_name('companyLocation').text)
+for i in range(0, 10):
+    url = 'https://uk.indeed.com/jobs?q=software%20engineer&l=london&limit=50&start'
+    url += str(50*i)
+    driver.get(url) 
+    jobs = driver.find_elements_by_class_name('tapItem')
+    for job in jobs:
+        companies.append(job.find_element_by_class_name('companyName').text)
+        titles.append(job.find_element_by_class_name('jobTitle').text)
+        addresses.append(job.find_element_by_class_name('companyLocation').text)
+        # ratings.append(job.find_element_by_class_name('ratingLink').text) @Todo [Neeraj]
+        try: 
+            salary = job.find_element_by_class_name('salary-snippet-container').text
+            salaries.append(salary)
+        except NoSuchElementException:
+            salaries.append('None')
 
-    try: 
-        salary = j.find_element_by_class_name('salary-snippet-container')
-        salaries.append(salary)
-    except NoSuchElementException:
-        salaries.append("None")
+        jobDescription.append(job.find_element_by_class_name('job-snippet').text)  
+    
 
-    jobDescription.append(j.find_element_by_class_name('job-snippet').text)   
-
-
-
-# results = pd.DataFrame({"job_title": titles,"company": companies})
-# results.head()
+res = pd.DataFrame({'Title': titles,'Company': companies, 'Address': addresses, 'Salary': salaries, 'Job description': jobDescription})
+print(res)
+res.to_csv('data.csv')
  
-driver.quit()
